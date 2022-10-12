d386da26c82ca673af2dbb002f97e6c4c0c22a42
dinesh <113573757+dineshselvaraj11@users.noreply.github.com>
2022-10-12 08:26:50 +0530

    Add files via upload

---
+++ luxe.java
@@ -0,0 +1,17 @@
+class Luxes
+{
+string moviename;
+int time;
+boolean hit;
+
+public Luxe(string mN,int t,boolean h)
+{
+
+system.out.println(movieName+"\n"+time+"\n"+hit);
+
+}
+public static void main(String[] args){
+Luxe s1=new Luex(nv,5,true);
+}
+}
+
